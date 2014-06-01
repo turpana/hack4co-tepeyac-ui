b7771b2e873434845edb87b6dc9ded73762bcc84
Turpana Molina <develop@turpana.com>
2014-05-31 19:29:21 -0600

    Adding api url info into users collection

--- app/scripts/collections/user.js
+++ app/scripts/collections/user.js
@@ -3,12 +3,16 @@
 define([
     'underscore',
     'backbone',
+    'config',
     'models/user'
-], function (_, Backbone, UserModel) {
+], function (_, Backbone, Config, UserModel) {
     'use strict';
 
     var UserCollection = Backbone.Collection.extend({
-        model: UserModel
+        model: UserModel,
+        url: function () {
+          return Config.api.host + '/api/user';
+        }
     });
 
     return UserCollection;
