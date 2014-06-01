0c5ae033ea080e749f9b01b4cc5b9357765dc43a
Turpana Molina <develop@turpana.com>
2014-05-31 22:59:18 -0600

    Calendar displaying messages on goal detail page

--- app/scripts/views/goal.js
+++ app/scripts/views/goal.js
@@ -5,18 +5,39 @@ define([
     'underscore',
     'backbone',
     'templates',
+    'fullcalendar',
     'views/base'
-], function ($, _, Backbone, JST, BaseView) {
+], function ($, _, Backbone, JST, FullCalendar, BaseView) {
     'use strict';
 
     var GoalView = BaseView.extend({
+        events: [],
         initialize: function (options) {
           this.model.messages.on('add', this.addMessage, this);
           this.model.messages.fetch();
         },
         template: JST['app/scripts/templates/goal.ejs'],
+        render: function () {
+          this.$el.html(this.template(this.model.attributes));
+          this.$el.find('.calendar').fullCalendar({
+            events: this.events
+          });
+          return this;
+        },
         addMessage: function (model, collection, options) {
-          console.log(model);
+          var date = new Date(model.get('created') * 1000);
+          var year = date.getFullYear();
+          var month = new String(1 + date.getMonth());
+          if (1 == month.length) month = "0" + month;
+          var date = new String(date.getDate());
+          if (1 == date.length) date = "0" + date;
+          var fullDate = year+'-'+month+'-'+date;
+          this.events.push({
+            start: fullDate,
+            title: model.responseStatus(),
+            color: model.color()
+          });
+          this.render();
         }
 
     });
