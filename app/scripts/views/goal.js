155b0ee272418e8a36cbcb937b15531ae9f72753
Turpana Molina <develop@turpana.com>
2014-06-01 01:37:45 -0600

    routing bug + nav and ux tweaks

--- app/scripts/views/goal.js
+++ app/scripts/views/goal.js
@@ -7,21 +7,40 @@ define([
     'templates',
     'utility',
     'fullcalendar',
+    'models/user',
     'views/base'
-], function ($, _, Backbone, JST, Utility, FullCalendar, BaseView) {
+], function ($, _, Backbone, JST, Utility, FullCalendar, UserModel, BaseView) {
     'use strict';
 
     var GoalView = BaseView.extend({
+        client: null,
         events: [],
         initialize: function (options) {
           this.model.messages.on('add', this.addMessage, this);
           this.model.messages.fetch();
+          this.client = new UserModel({
+            id: this.model.get('accountId')
+          });
+          this.client.fetch({
+            success: function(model, response, options) {
+              options.view.render();
+            },
+            view: this
+          });
         },
         template: JST['app/scripts/templates/goal.ejs'],
         render: function () {
+          var attributes = this.model.attributes;
+          attributes.clientName = this.client.get('firstName') + ' ' + this.client.get('lastName');
+          attributes.clientId = this.client.id;
           this.$el.html(this.template(this.model.attributes));
           this.$el.find('.calendar').fullCalendar({
-            events: this.events
+            events: this.events,
+            header: {
+              left: 'today',
+              center: 'title',
+              right:  'prev,next'
+            }
           });
           return this;
         },
